b1b2a661072f98bbded5d379af6842dae95cf38c
Unknown <arnaud.parrot@outlook.fr>
2018-06-08 16:01:23 +0200

    Premier Push du projet
    Ignoré tout ce qui (normalement) n'est pas nécessaire.

---
+++ FinalBootMVC/src/main/java/com/springboot/pkg/FinalBootMvcApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBootMvcApplication.class, args);
+	}
+}
