7331ba19ce32ce7df3e30567a2426208ada879a4
lereverendadeline <lereverend.adeline@gmail.com>
2018-06-11 15:21:20 +0200

    Enlevement de la security

--- FinalBootMVC/src/main/java/com/springboot/pkg/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.pkg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.pkg.model.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-	User findByUsername(String username);
-}
