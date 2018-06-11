b887d3525b0c306ee65d437e1bfda4505e7651a4
Unknown <arnaud.parrot@outlook.fr>
2018-06-11 10:37:27 +0200

    autre

--- FinalBootMVC/src/main/java/com/springboot/pkg/model/Stagiaire.java
+++ FinalBootMVC/src/main/java/com/springboot/pkg/model/Stagiaire.java
@@ -2,12 +2,15 @@ package com.springboot.pkg.model;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Version;
 
 @Entity
 @DiscriminatorValue(value = "S")
 public class Stagiaire extends RH {
 
 	private Ordinateur ordinateur;
+	@Version
+	private int version;
 	
 	public Stagiaire() {
 
