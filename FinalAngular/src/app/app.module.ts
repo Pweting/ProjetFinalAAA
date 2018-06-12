ee8313d1411b922b587999e0aab3ecb383501337
Unknown <arnaud.parrot@outlook.fr>
2018-06-12 11:11:30 +0200

    Modif ajout model+formation

--- FinalAngular/src/app/app.module.ts
+++ FinalAngular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { FormationComponent } from './formation/formation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    FormationComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(routes)
--- FinalAngular/src/app/routes.ts
+++ FinalAngular/src/app/routes.ts
@@ -1,7 +1,9 @@
+import { FormationComponent } from './formation/formation.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
+    { path: 'formation', component: FormationComponent },
 ];
