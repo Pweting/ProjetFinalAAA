77709f288b56324bcfd0feae1a5c66624f2d77b9
Pistou <alexandre27.perrault@gmail.com>
2018-06-12 11:28:17 +0200

    Component et routes

--- FinalAngular/src/app/app.module.ts
+++ FinalAngular/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { FormationComponent } from './formation/formation.component';
+import { RhComponent } from './rh/rh.component';
+import { MatiereComponent } from './matiere/matiere.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    FormationComponent
+    FormationComponent,
+    RhComponent,
+    MatiereComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(routes)
