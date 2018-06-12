e25a7d3c1d98e1258d12fdc665e954907c4f67ca
Unknown <arnaud.parrot@outlook.fr>
2018-06-12 12:32:06 +0200

    Service Formation complété

--- FinalAngular/src/app/services/formation.service.ts
+++ FinalAngular/src/app/services/formation.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Formation } from '../model/formation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormationService {
 
-  constructor() { }
+  private url: string = 'http://localhost:8080/finalaaa/rest/formation';
+
+  constructor(private http: HttpClient) { }
+
+  public list(): Observable<Formation[]> {
+    return this.http.get<Formation[]>(`${this.url}`);
+  }
+
+  public findById(id: number): Observable<Formation> {
+    return this.http.get<Formation>(`${this.url}/${id}`);
+  }
+
+  public FormationWithStagiaire(id: number): Observable<Formation> {
+    return this.http.get<Formation>(`${this.url}/{stagiaires}`);
+  }
+
+  public FormationWithModule(id: number): Observable<Formation> {
+    return this.http.get<Formation>(`${this.url}/{modules}`);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+
+  public save(f: Formation): Observable<any> {
+    return this.http.post<any>(`${this.url}/{formation}`, f);
+  }
+
+  public update(f: Formation): Observable<Formation> {
+    return this.http.put<Formation>(`${this.url}/{formation}`, f);
+  }
 }
