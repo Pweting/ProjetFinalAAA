5a9b536bf8d5ec77487ed73aaee2c2c84997d88b
Pistou <alexandre27.perrault@gmail.com>
2018-06-13 11:26:56 +0200

    Nouvelles routes

--- FinalAngular/src/app/routes.ts
+++ FinalAngular/src/app/routes.ts
@@ -1,23 +1,26 @@
-import { EditMaterielComponent } from './materiel/edit-materiel/edit-materiel.component';
-import { MaterielComponent } from './materiel/materiel.component';
-import { FormationComponent } from './formation/formation.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { DemoComponent } from './demo/component';
-import { MatiereComponent } from './matiere/matiere.component';
-import { RhComponent } from './rh/rh.component';
-import { Routes } from '@angular/router';
-import { EditComponent } from './formation/edit/edit.component';
+import {EditMaterielComponent} from './materiel/edit-materiel/edit-materiel.component';
+import {MaterielComponent} from './materiel/materiel.component';
+import {FormationComponent} from './formation/formation.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {DemoComponent} from './demo/component';
+import {MatiereComponent} from './matiere/matiere.component';
+import {RhComponent} from './rh/rh.component';
+import {Routes} from '@angular/router';
+import {EditComponent} from './formation/edit/edit.component';
+import { RhDetailComponent } from './rh/rh-detail.component';
 
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: 'formation', component: FormationComponent },
-    { path: 'formation/edit', component: EditComponent },
-    { path: 'formation/edit/:id', component: EditComponent },
-    { path: 'materiel/edit', component: EditMaterielComponent },
-    { path: 'materiel/edit/:id', component: EditMaterielComponent },
-    { path: 'matiere', component: MatiereComponent },
-    { path: 'rh', component: RhComponent },
-    { path: 'materiel', component: MaterielComponent },
-    { path: 'calendar', component: DemoComponent },
+  {path: '', component: DashboardComponent},
+  {path: 'formation', component: FormationComponent},
+  {path: 'formation/edit', component: EditComponent},
+  {path: 'formation/edit/:id', component: EditComponent},
+  {path: 'materiel/edit', component: EditMaterielComponent},
+  {path: 'materiel/edit/:id', component: EditMaterielComponent},
+  {path: 'matiere', component: MatiereComponent},
+  {path: 'rh', component: RhComponent},
+  {path: 'rh/edit', component: RhDetailComponent},
+  {path: 'rh/edit/:id', component: RhDetailComponent},
+  {path: 'materiel', component: MaterielComponent},
+  {path: 'calendar', component: DemoComponent},
 ];
